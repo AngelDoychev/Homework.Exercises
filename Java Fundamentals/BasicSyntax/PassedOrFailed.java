04118ab2fd60e7032db790d1194be48e87931a11
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-01-22 12:34:28 +0200

    Add files via upload

---
+++ Java Fundamentals/BasicSyntax/PassedOrFailed.java
@@ -0,0 +1,15 @@
+package FirstStepsInCoding.Fundamentals.BasicSyntax;
+
+import java.util.Scanner;
+
+public class PassedOrFailed {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double grade = Double.parseDouble(scanner.nextLine());
+        if (grade>=3){
+            System.out.println("Passed!");
+        }else {
+            System.out.println("Failed!");
+        }
+    }
+}
