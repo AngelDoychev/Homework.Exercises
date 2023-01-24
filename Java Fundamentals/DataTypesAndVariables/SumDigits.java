c1398d9cc1b8770208f2fe2c4faea9aeb13c2007
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-01-24 18:35:27 +0200

    Add files via upload

---
+++ Java Fundamentals/DataTypesAndVariables/SumDigits.java
@@ -0,0 +1,17 @@
+package FirstStepsInCoding.Fundamentals.DataTypesAmdVariables;
+
+import java.util.Scanner;
+
+public class SumDigits {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int num = Integer.parseInt(scanner.nextLine());
+        int sum = 0;
+        while(num>0){
+            int newNum = num % 10;
+            sum+=newNum;
+            num = num / 10;
+        }
+        System.out.println(sum);
+    }
+}
