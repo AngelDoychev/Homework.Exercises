ca3e109c8b3f394c9fb26ae1aa7e23f35e31f57d
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-01-24 18:38:14 +0200

    Add files via upload

---
+++ Java Fundamentals/DataTypesAndVariables/MoreExercises/FromLefttoTheRight.java
@@ -0,0 +1,28 @@
+package FirstStepsInCoding.Fundamentals.DataTypesAmdVariables.MoreExercises;
+
+import java.util.Scanner;
+
+public class FromLefttoTheRight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int inputLines = Integer.parseInt(scanner.nextLine());
+
+        for (int i = 0; i < inputLines; i++) {
+            String[] inputArray = scanner.nextLine().split(" ");
+            long firstNumber = Long.parseLong(inputArray[0]);
+            long secondNumber = Long.parseLong(inputArray[1]);
+
+            long currentNumber = Math.abs(Math.max(firstNumber, secondNumber));
+            long sumOfDigits = 0;
+            long currentDigit;
+
+            while (currentNumber > 0) {
+                currentDigit = (currentNumber % 10);
+                sumOfDigits = sumOfDigits + currentDigit;
+
+                currentNumber = currentNumber / 10;
+            }
+            System.out.println(sumOfDigits);
+        }
+    }
+}
