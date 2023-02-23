ff9e99f824df02405e09025772695a79350bc347
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-02-23 18:30:31 +0200

    Add files via upload

---
+++ Java Fundamentals/ObjectsAndClasses/SumBigNumbers.java
@@ -0,0 +1,14 @@
+package Fundametals.ObjectsAndClasses;
+
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class SumBigNumbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        BigInteger num1 = new BigInteger(scanner.nextLine());
+        BigInteger num2 = new BigInteger(scanner.nextLine());
+        BigInteger sum = num1.add(num2);
+        System.out.println(sum);
+    }
+}
