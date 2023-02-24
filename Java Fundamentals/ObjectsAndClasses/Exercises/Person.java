e9369740e42ccac23552e922d19bc43a965a1847
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-02-24 18:02:36 +0200

    Add files via upload

---
+++ Java Fundamentals/ObjectsAndClasses/Exercises/Person.java
@@ -0,0 +1,17 @@
+package Fundametals.ObjectsAndClasses.Exercises.OpinionPoll;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " - " + this.age;
+    }
+}
