fb03024c94015d2b7f8126c5c9ce37666af18192
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-04-13 17:51:23 +0300

    Add files via upload

---
+++ Java Advanced/StacksAndQueues/Exercise/RecursiveFibonacci.java
@@ -0,0 +1,25 @@
+package JavaAdvance.StacksAndQueues.Exercise;
+
+import java.util.ArrayDeque;
+import java.util.Scanner;
+
+public class RecursiveFibonacci {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = Integer.parseInt(scanner.nextLine());
+        if (n < 2) {
+            System.out.println(1);
+            return;
+        }
+        ArrayDeque<Long> stack = new ArrayDeque<>();
+        stack.push(0l);
+        stack.push(1l);
+        for (int i = 0; i < n; i++) {
+            long num1 = stack.pop();
+            long num2 = stack.pop();
+            stack.push(num1);
+            stack.push(num1 + num2);
+        }
+        System.out.println(stack.pop());
+    }
+}
