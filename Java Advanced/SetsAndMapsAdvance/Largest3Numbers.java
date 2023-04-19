cc5efea7fbdec6d4d4833e6720279c7ecb72d02c
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-04-19 20:54:52 +0300

    Add files via upload

---
+++ Java Advanced/SetsAndMapsAdvance/Largest3Numbers.java
@@ -0,0 +1,16 @@
+package JavaAdvance.SetsAndMapsAdvanced;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.Scanner;
+
+public class Largest3Numbers {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Arrays.stream(scanner.nextLine().split("\\s+"))
+                .map(Integer::parseInt)
+                .sorted(Comparator.reverseOrder())
+                .limit(3).
+                forEach(number -> System.out.print(number + " "));
+    }
+}
