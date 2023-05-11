c9016e58f70686074131396d63a08e55e78d77c0
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-05-11 20:28:41 +0300

    Add files via upload

---
+++ Java Advanced/DefiningClasses/Exercise/Google/Parents.java
@@ -0,0 +1,27 @@
+package JavaAdvance.DefiningClasses.Exercise.Google;
+
+public class Parents {
+    private String name = "";
+    private String birthday = "";
+
+    @Override
+    public String toString() {
+        return String.format("%s %s", this.name, this.birthday);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+}
