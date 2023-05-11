ca52e8b5e04e235e593557dce423970764c605f3
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-05-11 20:26:17 +0300

    Add files via upload

---
+++ Java Advanced/DefiningClasses/Exercise/Employee.java
@@ -0,0 +1,81 @@
+package JavaAdvance.DefiningClasses.Exercise.CompanyRoster;
+
+public class Employee {
+    private String name;
+    private double salary;
+    private String position;
+    private String department;
+    private String email = "n/a";
+    private int age = -1;
+
+    public Employee(String name, double salary, String position, String department) {
+        this.name = name;
+        this.salary = salary;
+        this.position = position;
+        this.department = department;
+    }
+
+    public Employee(String name, double salary, String position, String department, String email) {
+        this(name, salary, position, department);
+        this.email = email;
+    }
+
+    public Employee(String name, double salary, String position, String department, int age) {
+        this(name, salary, position, department);
+        this.age = age;
+    }
+
+    public Employee(String name, double salary, String position, String department, String email, int age) {
+        this(name, salary, position, department);
+        this.email = email;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
