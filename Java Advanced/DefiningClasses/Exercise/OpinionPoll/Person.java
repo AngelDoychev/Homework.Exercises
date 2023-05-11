bdf0353d78ccf39fd0518920b8737006688aa6fe
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-05-11 20:29:49 +0300

    Add files via upload

---
+++ Java Advanced/DefiningClasses/Exercise/OpinionPoll/Person.java
@@ -0,0 +1,22 @@
+package JavaAdvance.DefiningClasses.Exercise.OpinionPoll;
+
+public class Person {
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
