3596941c8c29472e114a2c3fcc43bab68fe3946e
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-05-26 20:24:25 +0300

    Add files via upload

---
+++ Java Advanced/Generics/Exercise/Box.java
@@ -0,0 +1,31 @@
+package JavaAdvance.Generics.Exercise.GenericSwapMethodInteger;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T> {
+    private List<T> list;
+
+    public Box() {
+        this.list = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        this.list.add(element);
+    }
+
+    public void swap(int num1, int num2) {
+        T temp = this.list.get(num1);
+        this.list.set(num1, this.list.get(num2));
+        this.list.set(num2, temp);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T value : list) {
+            sb.append(String.format("%s: %s%n", value.getClass().getName(), value.toString()));
+        }
+        return sb.toString();
+    }
+}
