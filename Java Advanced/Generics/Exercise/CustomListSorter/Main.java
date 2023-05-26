f25a2223baf962f1ee93e4531e600c824686b21d
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-05-26 20:19:57 +0300

    Add files via upload

---
+++ Java Advanced/Generics/Exercise/CustomListSorter/Main.java
@@ -0,0 +1,43 @@
+package JavaAdvance.Generics.Exercise.CustomListSorter;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] command = scanner.nextLine().split("\\s+");
+        CustomList<String> customList = new CustomList<>();
+        while (!command[0].equals("END")) {
+            switch (command[0]) {
+                case "Add":
+                    customList.add(command[1]);
+                    break;
+                case "Sort":
+                    Sorter.sort(customList);
+                    break;
+                case "Remove":
+                    customList.remove(Integer.parseInt(command[1]));
+                    break;
+                case "Contains":
+                    System.out.println(customList.contains(command[1]));
+                    break;
+                case "Swap":
+                    customList.swap(Integer.parseInt(command[1]), Integer.parseInt(command[2]));
+                    break;
+                case "Greater":
+                    System.out.println(customList.countGreaterThan(command[1]));
+                    break;
+                case "Max":
+                    System.out.println(customList.getMax());
+                    break;
+                case "Min":
+                    System.out.println(customList.getMin());
+                    break;
+                case "Print":
+                    System.out.println(customList);
+                    break;
+            }
+            command = scanner.nextLine().split("\\s+");
+        }
+    }
+}
