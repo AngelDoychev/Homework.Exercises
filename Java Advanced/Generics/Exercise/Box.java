40d7ebbc83266d31cc95432c871315ec943a28eb
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-05-26 20:23:32 +0300

    Add files via upload

---
+++ Java Advanced/Generics/Exercise/Box.java
@@ -0,0 +1,27 @@
+package JavaAdvance.Generics.Exercise.GenericCountMethodStrings;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box<T extends Comparable<T>> {
+    private List<T> list;
+
+    public Box() {
+        this.list = new ArrayList<>();
+    }
+
+    public void add(T element) {
+        this.list.add(element);
+    }
+public int count(T element){
+        return (int) list.stream().filter(e->e.compareTo(element) > 0).count();
+}
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (T value : list) {
+            sb.append(String.format("%s: %s%n", value.getClass().getName(), value.toString()));
+        }
+        return sb.toString();
+    }
+}
