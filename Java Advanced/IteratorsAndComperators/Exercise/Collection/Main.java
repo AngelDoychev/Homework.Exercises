5da2b41d8aed398569ed34ba07932029e01c2879
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-06 19:53:26 +0300

    Add files via upload

---
+++ Java Advanced/IteratorsAndComperators/Exercise/Collection/Main.java
@@ -0,0 +1,45 @@
+package JavaAdvance.IteratorsAndComperators.Exercise.Collection;
+
+
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String[] command = scanner.nextLine().split("\\s+");
+        ListyIterator current = new ListyIterator();
+        while (!command[0].equals("END")) {
+            switch (command[0]) {
+                case "Create":
+                    if (command.length>1) {
+                        String[] temp = new String[command.length - 1];
+                        for (int i = 1; i < command.length; i++) {
+                            temp[i-1] = command[i];
+                        }
+                        current = new ListyIterator(temp);
+                    }else {
+                        current = new ListyIterator();
+                    }
+                    break;
+                case "Move":
+                        System.out.println(current.next());
+                    break;
+                case "HasNext":
+                    System.out.println(current.hasNext());
+                    break;
+                case "Print":
+                    if (current.getLength()==0){
+                        System.out.println("Invalid Operation!");
+                    }else {
+                        System.out.println(current.getCurrent());
+                    }
+                    break;
+                case"PrintAll":
+                    current.printAll();
+                    break;
+            }
+            command = scanner.nextLine().split("\\s+");
+        }
+    }
+}
