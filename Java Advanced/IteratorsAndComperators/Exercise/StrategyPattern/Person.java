0166b048916471b65a694b65ed558684f53c873c
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-06 19:59:25 +0300

    Add files via upload

---
+++ Java Advanced/IteratorsAndComperators/Exercise/StrategyPattern/Person.java
@@ -0,0 +1,37 @@
+package JavaAdvance.IteratorsAndComperators.Exercise.StrategyPattern;
+
+public class Person implements Comparable<Person> {
+    private String name;
+    private int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %d", this.name, this.age);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public int compareTo(Person o) {
+        return 0;
+    }
+}
