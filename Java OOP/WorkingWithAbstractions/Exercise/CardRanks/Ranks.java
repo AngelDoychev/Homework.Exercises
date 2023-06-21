2f3c473743b0e0aa74110074dd2efc82f3e2e59d
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-21 18:33:41 +0300

    Add files via upload

---
+++ Java OOP/WorkingWithAbstractions/Exercise/CardRanks/Ranks.java
@@ -0,0 +1,17 @@
+package JavaOOP.WorkingWithAbstraction.Exercise.CardRanks;
+
+public enum Ranks {
+    ACE,
+    TWO,
+    THREE,
+    FOUR,
+    FIVE,
+    SIX,
+    SEVEN,
+    EIGHT,
+    NINE,
+    TEN,
+    JACK,
+    QUEEN,
+    KING;
+}
