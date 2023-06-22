36ef15a4098d5792254224c2c15d4a87c8fdbcd8
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-22 17:47:59 +0300

    Add files via upload

---
+++ Java OOP/Encapsulation/SortByNameAndAge/Person.java
@@ -0,0 +1,30 @@
+package JavaOOP.Encapsulation.SortByNameAndAge;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Person(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s is %d years old.", this.firstName, this.lastName, this.age);
+    }
+}
