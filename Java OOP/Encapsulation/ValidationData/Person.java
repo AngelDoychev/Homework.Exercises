8ddf52dbd842bd672a5df36c2f518d4ed8a2a868
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-22 17:48:42 +0300

    Add files via upload

---
+++ Java OOP/Encapsulation/ValidationData/Person.java
@@ -0,0 +1,72 @@
+package JavaOOP.Encapsulation.ValidationData;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        setFirstName(firstName);
+        setLastName(lastName);
+        setAge(age);
+        setSalary(salary);
+    }
+
+    public void increaseSalary(double bonus) {
+        if (this.age >= 30) {
+            this.setSalary(this.getSalary() + (this.getSalary() * bonus / 100));
+        } else {
+            this.setSalary(this.getSalary() + (this.getSalary() * bonus / 200));
+        }
+    }
+
+    public void setFirstName(String firstName) {
+        if (firstName.length()<3){
+            throw new IllegalArgumentException("First name cannot be less than 3 symbols");
+        }
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        if (lastName.length()<3){
+            throw new IllegalArgumentException("Last name cannot be less than 3 symbols");
+        }
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        if (age<1){
+            throw new IllegalArgumentException("Age cannot be zero or negative integer");
+        }
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        if (salary < 460) {
+            throw new IllegalArgumentException("Salary cannot be less than 460 leva");
+        }
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s %s gets %f leva", this.firstName, this.lastName, this.salary);
+    }
+}
