9693115d985f35f83ff1529463568ea2fe6392a9
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-23 21:10:42 +0300

    Add files via upload

---
+++ Java OOP/Encapsulation/Exercise/ShoppingSpree/Product.java
@@ -0,0 +1,38 @@
+package JavaOOP.Encapsulation.Exercise.ShoppingSpree;
+
+public class Product {
+    private String name;
+    private double cost;
+
+    public Product(String name, double cost) {
+        setName(name);
+        setCost(cost);
+    }
+
+    @Override
+    public String toString() {
+        return this.getName();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    private void setName(String name) {
+        if (name == null || name.trim().isEmpty()) {
+            throw new IllegalArgumentException("Name cannot be empty");
+        }
+        this.name = name;
+    }
+
+    private void setCost(double cost) {
+        if (cost < 0) {
+            throw new IllegalArgumentException("Money cannot be negative");
+        }
+        this.cost = cost;
+    }
+}
