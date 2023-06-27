18464f1b24179988f618c0ccde48f04c2370fd87
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-27 14:31:32 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/SingleInheritance/Main.java
@@ -0,0 +1,9 @@
+package JavaOOP.Inheritance.SingleInheritance;
+
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.eat();
+        dog.bark();
+    }
+}
