b1d9b98c96f7aad90f0224c032dbc9b53362fbc7
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-28 16:24:48 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/Exercise/PlayersAndMonsters/MuseElf.java
@@ -0,0 +1,7 @@
+package JavaOOP.Inheritance.Exercise.PlayersAndMonsters;
+
+public class MuseElf extends Elf {
+    public MuseElf(String username, int level) {
+        super(username, level);
+    }
+}
