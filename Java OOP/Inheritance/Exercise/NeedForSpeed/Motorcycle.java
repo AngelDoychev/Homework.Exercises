89d33931db28400b0b913b05a750d331a1eb5e7d
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-28 16:19:52 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/Exercise/NeedForSpeed/Motorcycle.java
@@ -0,0 +1,7 @@
+package JavaOOP.Inheritance.Exercise.NeedForSpeed;
+
+public class Motorcycle extends Vehicle {
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
