22cde5329bae73c46c9dfb95bb2c62c6eb465c61
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-28 16:16:55 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/Exercise/Animals/Dog.java
@@ -0,0 +1,10 @@
+package JavaOOP.Inheritance.Exercise.animals;
+
+public class Dog extends Animal{
+    public Dog(String name, int age, String gender) {
+        super(name, age, gender);
+    }
+    public String produceSound(){
+        return "Woof!";
+    }
+}
