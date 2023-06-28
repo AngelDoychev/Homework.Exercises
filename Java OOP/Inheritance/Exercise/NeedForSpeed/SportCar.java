f728fd50919739df7ad266ff1d9707319f3d5628
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-28 16:20:24 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/Exercise/NeedForSpeed/SportCar.java
@@ -0,0 +1,9 @@
+package JavaOOP.Inheritance.Exercise.NeedForSpeed;
+
+public class SportCar extends Car{
+    public final static double DEFAULT_FUEL_CONSUMPTION = 10;
+    public SportCar(double fuel, int horsePower) {
+        super(fuel, horsePower);
+super.setFuelConsumption(DEFAULT_FUEL_CONSUMPTION);
+    }
+}
