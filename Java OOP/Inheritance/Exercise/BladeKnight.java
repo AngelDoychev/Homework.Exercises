09334e6606d612b6f970cb8c48ae75b1a4e796e3
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-28 16:22:27 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/Exercise/BladeKnight.java
@@ -0,0 +1,7 @@
+package JavaOOP.Inheritance.Exercise.PlayersAndMonsters;
+
+public class BladeKnight extends DarkKnight {
+    public BladeKnight(String username, int level) {
+        super(username, level);
+    }
+}
