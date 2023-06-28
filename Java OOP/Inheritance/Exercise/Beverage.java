22e57cf5bae13e4f55e65268a5ab08fc66655e37
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-28 16:26:04 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/Exercise/Beverage.java
@@ -0,0 +1,15 @@
+package JavaOOP.Inheritance.Exercise.restaurant;
+
+import java.math.BigDecimal;
+
+public class Beverage extends Product{
+    private double milliliters;
+    public Beverage(String name, BigDecimal price, double milliliters) {
+        super(name, price);
+        this.milliliters = milliliters;
+    }
+
+    public double getMilliliters() {
+        return milliliters;
+    }
+}
