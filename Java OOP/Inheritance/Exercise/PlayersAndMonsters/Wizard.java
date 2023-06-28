4c98ec4b5dd6591681e16dc1029f1f844097eecc
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-28 16:25:26 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/Exercise/PlayersAndMonsters/Wizard.java
@@ -0,0 +1,7 @@
+package JavaOOP.Inheritance.Exercise.PlayersAndMonsters;
+
+public class Wizard extends Hero{
+    public Wizard(String username, int level) {
+        super(username, level);
+    }
+}
