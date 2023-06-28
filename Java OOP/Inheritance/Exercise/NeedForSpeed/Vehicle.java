305653957139da35765ad91fae538214a66c2891
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-28 16:20:39 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/Exercise/NeedForSpeed/Vehicle.java
@@ -0,0 +1,46 @@
+package JavaOOP.Inheritance.Exercise.NeedForSpeed;
+
+public class Vehicle {
+    final static double DEFAULT_FUEL_CONSUMPTION = 1.25;
+    private double fuelConsumption;
+    private double fuel;
+    private int horsePower;
+
+    public Vehicle(double fuel, int horsePower) {
+        this.fuelConsumption = DEFAULT_FUEL_CONSUMPTION;
+        this.fuel = fuel;
+        this.horsePower = horsePower;
+    }
+
+    public void drive(double kilometers) {
+        if (fuel >= kilometers * fuelConsumption) {
+            fuel -= kilometers * fuelConsumption;
+        } else {
+            return;
+        }
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+
+    public double getFuel() {
+        return fuel;
+    }
+
+    public void setFuel(double fuel) {
+        this.fuel = fuel;
+    }
+
+    public int getHorsePower() {
+        return horsePower;
+    }
+
+    public void setHorsePower(int horsePower) {
+        this.horsePower = horsePower;
+    }
+}
