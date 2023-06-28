3ba3c0af285b1522b3eb9edda832fe95e0dfc3c1
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-28 16:29:39 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/Exercise/Restaurant/Soup.java
@@ -0,0 +1,9 @@
+package JavaOOP.Inheritance.Exercise.restaurant;
+
+import java.math.BigDecimal;
+
+public class Soup extends Starter {
+    public Soup(String name, BigDecimal price, double grams) {
+        super(name, price, grams);
+    }
+}
