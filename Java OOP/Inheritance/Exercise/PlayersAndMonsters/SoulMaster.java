9318a31a8a5379cf63a11b2598403dc265a1ffc1
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-28 16:25:05 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/Exercise/PlayersAndMonsters/SoulMaster.java
@@ -0,0 +1,8 @@
+package JavaOOP.Inheritance.Exercise.PlayersAndMonsters;
+
+
+public class SoulMaster extends DarkWizard {
+    public SoulMaster(String username, int level) {
+        super(username, level);
+    }
+}
