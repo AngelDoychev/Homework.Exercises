4feb7e830db93bd1027fd24d1ccac1454e51d8d2
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-28 16:32:23 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/Exercise/Zoo/Main.java
@@ -0,0 +1,10 @@
+package JavaOOP.Inheritance.Exercise.Zoo;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+    }
+}
