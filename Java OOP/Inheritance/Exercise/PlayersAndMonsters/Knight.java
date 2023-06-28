da3399024f11216e42a6f5b3ab5d64ec308dd5df
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-28 16:24:14 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/Exercise/PlayersAndMonsters/Knight.java
@@ -0,0 +1,7 @@
+package JavaOOP.Inheritance.Exercise.PlayersAndMonsters;
+
+public class Knight extends Hero {
+    public Knight(String username, int level) {
+        super(username, level);
+    }
+}
