a26d00d39d376586c79c15109d526b0382fa73bc
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-28 16:30:24 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/Exercise/Restaurant/Tea.java
@@ -0,0 +1,9 @@
+package JavaOOP.Inheritance.Exercise.restaurant;
+
+import java.math.BigDecimal;
+
+public class Tea extends HotBeverage {
+    public Tea(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
