871648b32aa0202819b5d021d783951a24324eab
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-28 16:17:30 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/Exercise/Animals/Kitten.java
@@ -0,0 +1,11 @@
+package JavaOOP.Inheritance.Exercise.animals;
+
+public class Kitten extends Cat{
+    public Kitten(String name, int age) {
+        super(name, age, "Female");
+    }
+    @Override
+    public String produceSound(){
+        return "Meow";
+    }
+}
