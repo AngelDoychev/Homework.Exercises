ffd4bcca1db6f6466e4c4181530124da20df4d64
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-28 16:23:22 +0300

    Add files via upload

---
+++ Java OOP/Inheritance/Exercise/PlayersAndMonsters/DarkWizard.java
@@ -0,0 +1,7 @@
+package JavaOOP.Inheritance.Exercise.PlayersAndMonsters;
+
+public class DarkWizard extends Wizard{
+    public DarkWizard(String username, int level) {
+        super(username, level);
+    }
+}
