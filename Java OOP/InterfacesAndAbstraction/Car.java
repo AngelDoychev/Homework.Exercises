07319ceeb3e564b3bac7d9741c89e78f96e2eccc
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-29 18:47:25 +0300

    Add files via upload

---
+++ Java OOP/InterfacesAndAbstraction/Car.java
@@ -0,0 +1,9 @@
+package JavaOOP.IntefacesAndAbstraction.CarShop;
+
+public interface Car {
+     int TIRES = 4;
+     String getModel();
+     String getColor();
+     Integer getHorsePower();
+     String countryProduced();
+}
