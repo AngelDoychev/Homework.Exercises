3a3ab23f53c04925a9cc1a7e28597ccb093afb0b
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-29 18:52:55 +0300

    Add files via upload

---
+++ Java OOP/InterfacesAndAbstraction/SayHelloExtended/Chinese.java
@@ -0,0 +1,12 @@
+package JavaOOP.IntefacesAndAbstraction.SayHelloExtended;
+
+public class Chinese extends BasePerson{
+    private String name;
+
+    public Chinese(String name) {
+        super(name);
+    }
+    public String sayHello() {
+        return "Djydjybydjy";
+    }
+}
