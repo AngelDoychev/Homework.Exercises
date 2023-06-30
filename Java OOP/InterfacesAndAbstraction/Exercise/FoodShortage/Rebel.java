e190b6016a1b3d9bbe70f170005bcc0a41d87e42
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-30 16:57:09 +0300

    Add files via upload

---
+++ Java OOP/InterfacesAndAbstraction/Exercise/FoodShortage/Rebel.java
@@ -0,0 +1,39 @@
+package JavaOOP.IntefacesAndAbstraction.Exercise.FoodShortage;
+
+public class Rebel implements Buyer, Person {
+    private String name;
+    private int age;
+    private String group;
+    private int food = 0;
+
+    public Rebel(String name, int age, String group) {
+        this.name = name;
+        this.age = age;
+        this.group = group;
+    }
+
+    public String getGroup() {
+        return this.group;
+    }
+
+    @Override
+    public void buyFood() {
+        this.food += 5;
+    }
+
+    @Override
+    public int getFood() {
+        return this.food;
+    }
+
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+}
