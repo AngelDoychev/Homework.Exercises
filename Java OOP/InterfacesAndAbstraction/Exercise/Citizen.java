de618df24204715d5e88a607351eaaf764372fb3
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-30 16:54:24 +0300

    Add files via upload

---
+++ Java OOP/InterfacesAndAbstraction/Exercise/Citizen.java
@@ -0,0 +1,21 @@
+package JavaOOP.IntefacesAndAbstraction.Exercise.DefineAnInterfacePerson;
+
+public class Citizen implements Person{
+    private String name;
+    private int age;
+
+    public Citizen(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public int getAge() {
+        return this.age;
+    }
+}
