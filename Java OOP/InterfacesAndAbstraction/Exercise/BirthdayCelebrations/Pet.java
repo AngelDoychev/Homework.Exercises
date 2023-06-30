78d71310675508db16053d1993809f8412e2373e
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-06-30 16:53:38 +0300

    Add files via upload

---
+++ Java OOP/InterfacesAndAbstraction/Exercise/BirthdayCelebrations/Pet.java
@@ -0,0 +1,23 @@
+package JavaOOP.IntefacesAndAbstraction.Exercise.BirthdayCelebrations;
+
+
+
+public class Pet implements Birthable {
+    private String name;
+    private String birthDate;
+
+    public Pet(String name, String birthDate) {
+        this.name = name;
+        this.birthDate = birthDate;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public String getBirthDate() {
+        return this.birthDate;
+    }
+
+}
