8e650d4f118a64c8585fce0ab1bb30909eee9cbb
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-07-04 18:51:49 +0300

    Add files via upload

---
+++ Java OOP/Polymorphism/Animals/Dog.java
@@ -0,0 +1,12 @@
+package JavaOOP.Polymorphism.Animals;
+
+public class Dog extends Animal{
+    public Dog(String name, String favouriteFood) {
+        super(name, favouriteFood);
+    }
+
+    @Override
+    public String explainSelf() {
+        return String.format("I am %s and my favourite food is %s%nDJAAF", super.getName(), super.getFavouriteFood());
+    }
+}
