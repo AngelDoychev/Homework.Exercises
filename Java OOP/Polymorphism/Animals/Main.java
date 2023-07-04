b68b458df7259386e35c9d0377f7eef9110e1a1d
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-07-04 18:52:04 +0300

    Add files via upload

---
+++ Java OOP/Polymorphism/Animals/Main.java
@@ -0,0 +1,10 @@
+package JavaOOP.Polymorphism.Animals;
+
+public class Main {
+    public static void main(String[] args) {
+        Animal cat = new Cat("Oscar", "Whiskas");
+        Animal dog = new Dog("Rocky", "Meat");
+        System.out.println(cat.explainSelf());
+        System.out.println(dog.explainSelf());
+    }
+}
