c4860daf41d75a837b5f92c1ba4534396c7ba0b4
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-07-04 18:50:18 +0300

    Add files via upload

---
+++ Java OOP/Animal.java
@@ -0,0 +1,21 @@
+package JavaOOP.Polymorphism.Animals;
+
+public abstract class Animal {
+    private String name;
+    private String favouriteFood;
+
+    public Animal(String name, String favouriteFood) {
+        this.name = name;
+        this.favouriteFood = favouriteFood;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getFavouriteFood() {
+        return favouriteFood;
+    }
+
+    public abstract String explainSelf();
+}
