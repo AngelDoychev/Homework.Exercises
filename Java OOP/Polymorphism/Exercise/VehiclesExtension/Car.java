6c7a69fb06291c3338631f6746c0b49b59e6ff69
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-07-05 20:12:43 +0300

    Add files via upload

---
+++ Java OOP/Polymorphism/Exercise/VehiclesExtension/Car.java
@@ -0,0 +1,14 @@
+package JavaOOP.Polymorphism.Exercise.VehiclesExtension;
+
+public class Car extends VehicleImpl {
+    private static final double AC_ADDITIONAL_CONSUMPTION = 0.9;
+
+    public Car(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption, tankCapacity);
+    }
+
+    @Override
+    public void setFuelConsumption(double fuelConsumption) {
+        super.setFuelConsumption(fuelConsumption + AC_ADDITIONAL_CONSUMPTION);
+    }
+}
