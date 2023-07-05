96999e3e310a9ab0e20a413de6bb8a1a9c363fc5
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-07-05 20:13:55 +0300

    Add files via upload

---
+++ Java OOP/Polymorphism/Exercise/VehiclesExtension/VehicleImpl.java
@@ -0,0 +1,62 @@
+package JavaOOP.Polymorphism.Exercise.VehiclesExtension;
+
+import java.text.DecimalFormat;
+
+public class VehicleImpl implements Vehicle{
+    private double fuelQuantity;
+    private double fuelConsumption;
+    private double tankCapacity;
+
+    protected VehicleImpl(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        this.fuelQuantity = fuelQuantity;
+        this.setFuelConsumption(fuelConsumption);
+        this.tankCapacity = tankCapacity;
+    }
+
+    @Override
+    public String drive(double distance) {
+        double fuelNeeded = distance * this.getFuelConsumption();
+        if (fuelNeeded > this.fuelQuantity) {
+            return this.getClass().getSimpleName() + " needs refueling";
+        }
+        this.fuelQuantity -= fuelNeeded;
+
+        DecimalFormat formatter = new DecimalFormat("##.##");
+        return String.format("%s travelled %s km", this.getClass().getSimpleName(), formatter.format(distance));
+    }
+
+    @Override
+    public void refuel(double liters) {
+        if(liters <= 0 ) {
+            throw new IllegalArgumentException("Fuel must be a positive number");
+        }
+
+        double newFuelQuantity = this.fuelQuantity + liters;
+        if(newFuelQuantity > this.tankCapacity){
+            throw new IllegalArgumentException("Cannot fit fuel in tank");
+        }
+
+        this.fuelQuantity += liters;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s: %.2f", this.getClass().getSimpleName(), this.fuelQuantity);
+    }
+
+    public double getFuelQuantity() {
+        return fuelQuantity;
+    }
+
+    public void setFuelQuantity(double fuelQuantity) {
+        this.fuelQuantity = fuelQuantity;
+    }
+
+    public double getFuelConsumption() {
+        return fuelConsumption;
+    }
+
+    public void setFuelConsumption(double fuelConsumption) {
+        this.fuelConsumption = fuelConsumption;
+    }
+}
