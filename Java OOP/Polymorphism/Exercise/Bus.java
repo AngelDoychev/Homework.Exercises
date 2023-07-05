31254635213400b6009137714d0e381f4ffbb328
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-07-05 20:12:13 +0300

    Add files via upload

---
+++ Java OOP/Polymorphism/Exercise/Bus.java
@@ -0,0 +1,29 @@
+package JavaOOP.Polymorphism.Exercise.VehiclesExtension;
+
+import java.text.DecimalFormat;
+
+public class Bus extends VehicleImpl {
+    private static final double AC_ADDITIONAL_CONSUMPTION = 1.4;
+    private boolean isEmpty;
+
+    public Bus(double fuelQuantity, double fuelConsumption, double tankCapacity) {
+        super(fuelQuantity, fuelConsumption, tankCapacity);
+        isEmpty = true;
+    }
+
+    public void setEmpty(boolean empty) {
+        if (this.isEmpty == empty) {
+            return;
+        }
+
+        if(this.isEmpty && !empty){
+            super.setFuelConsumption(super.getFuelConsumption() + AC_ADDITIONAL_CONSUMPTION);
+        }
+
+        if(!this.isEmpty && empty){
+            super.setFuelConsumption(super.getFuelConsumption() - AC_ADDITIONAL_CONSUMPTION);
+        }
+
+        this.isEmpty = empty;
+    }
+}
