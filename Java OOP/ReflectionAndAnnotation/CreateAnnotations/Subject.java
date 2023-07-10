24298e673a9a32256ef9c0124dbcd75f8e0af88b
Angel Doychev <120913858+AngelDoychev@users.noreply.github.com>
2023-07-10 18:04:09 +0300

    Add files via upload

---
+++ Java OOP/ReflectionAndAnnotation/CreateAnnotations/Subject.java
@@ -0,0 +1,11 @@
+package JavaOOP.Reflection.CreateAnnotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface Subject {
+    String[] categories();
+}
